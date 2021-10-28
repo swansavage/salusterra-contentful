7c9a33c0e831e3089ec262bbb6b067d84e579a43
swansavage@github.com <swansavage@gmail.com>
2021-10-28 13:09:39 -0500

    removed remaining 'tags' from graphql queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,6 @@ export const pageQuery = graphql`
         title
         slug
         publishDate(formatString: "MMMM Do, YYYY")
-        tags
         heroImage {
           gatsbyImageData(
             layout: FULL_WIDTH
